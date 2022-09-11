f2f282dd4dd752e98bfb038ef9264c3e819f6371
Hyormone <102273370+Hyormone@users.noreply.github.com>
2022-09-11 23:33:11 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 520 ms, Memory: 73120 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(input())
+dic = dict()
+
+for i in range(n):
+    dic[i] = list(map(int, sys.stdin.readline().rstrip().split()))
+
+dic_sort = sorted(dic.items(), key=lambda x:(x[1][0],x[1][1]))
+
+for item in dic_sort:
+    print(*item[1])
