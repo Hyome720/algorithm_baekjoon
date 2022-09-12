96d6cac141f790669c452077d0f5bfd415ad7361
Hyormone <102273370+Hyormone@users.noreply.github.com>
2022-09-12 16:45:18 +0900

    [Silver III] Title: N과 M (3), Time: 2068 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,15 @@
+import sys
+
+def sequence(n, m, res):
+    if len(res) == m:
+        print(*res)
+        return
+    for i in range(1, n + 1):
+        res.append(i)
+        sequence(n, m, res)
+        res.pop()
+
+
+n, m = map(int, input().split())
+
+sequence(n, m, [])
