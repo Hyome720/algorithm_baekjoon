d868b5b3e5e24b31ac62b192fbbf82e4a8518f51
Hyormone <102273370+Hyormone@users.noreply.github.com>
2022-09-15 13:35:05 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 676 ms, Memory: 59680 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+n, m, r = map(int, sys.stdin.readline().rstrip().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+
+q = deque()
+q.append(r)
+
+lst = [0] * (n + 1)
+lst[r] = 1
+res = [0] * (n + 1)
+res[r] = 1
+cnt = 1
+
+def bfs():
+    global cnt
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if not lst[i]:
+                if not res[i]:
+                    cnt += 1
+                    res[i] = cnt
+                lst[i] = now
+                q.append(i)
+
+bfs()
+
+for i in res[1:]:
+    print(i)
