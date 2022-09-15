cba236267f355e19c3f8d4714a78be0236dadd86
Hyormone <102273370+Hyormone@users.noreply.github.com>
2022-09-15 10:59:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 688 ms, Memory: 159788 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,35 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+n, m, r = map(int, sys.stdin.readline().rstrip().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort(reverse=True)
+
+visited = [0] * (n + 1)
+visited[r] = 1
+
+lst = [0] * (n + 1)
+lst[r] = 1
+cnt = 1
+
+def dfs(t):
+    global cnt
+    for i in graph[t]:
+        if not visited[i]:
+            cnt += 1
+            visited[i] = t
+            lst[i] = cnt
+            dfs(i)
+
+dfs(r)
+
+for i in lst[1:]:
+    print(i)
