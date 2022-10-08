4fa7464d361568ab324ae2c7a524239a65e013b6
Hyormone <102273370+Hyormone@users.noreply.github.com>
2022-10-08 22:50:54 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+lst = []
+for i in range(n):
+    lst.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+for i in range(1, n):
+    lst[i][0] = min(lst[i - 1][1], lst[i - 1][2]) + lst[i][0]
+    lst[i][1] = min(lst[i - 1][0], lst[i - 1][2]) + lst[i][1]
+    lst[i][2] = min(lst[i - 1][0], lst[i - 1][1]) + lst[i][2]
+
+print(min(lst[n - 1][0], lst[n - 1][1], lst[n - 1][2]))
